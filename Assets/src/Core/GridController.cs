4fec01c12d23fda66b5e53ed75c81a9cd4f1daa6
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-09-04 18:32:35 +0300

    TOW-43 add track construction algorithm

--- Assets/src/Core/GridController.cs
+++ Assets/src/Core/GridController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GridController : MonoBehaviour
 {
@@ -19,6 +20,9 @@ public class GridController : MonoBehaviour
     void GenerateGrid()
     {
         grid = new GameObject[gridSizeX, gridSizeY];
+
+        List<Vector2Int> path = FindPath(); 
+
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int y = 0; y < gridSizeY; y++)
@@ -27,16 +31,14 @@ public class GridController : MonoBehaviour
                 GameObject newNode = Instantiate(nodePrefab, nodePosition, Quaternion.identity);
                 Node nodeComponent = newNode.AddComponent<Node>();
 
-                if (x % 2 == 0)
+                if (path.Contains(new Vector2Int(x, y)))
                 {
-                    nodeComponent.spriteRenderer.sprite = sprites[0];
-                    nodeComponent.haveSomething = true;
+                    nodeComponent.spriteRenderer.sprite = sprites[0]; 
                 }
-                else if (x % 2 > 0) {
-                    nodeComponent.spriteRenderer.sprite = sprites[1];
+                else
+                {
+                    nodeComponent.spriteRenderer.sprite = sprites[1]; 
                 }
-                   
-
 
                 newNode.gameObject.name = "X: " + nodePosition.x + " Y: " + nodePosition.y;
                 nodeComponent.x = x;
@@ -46,5 +48,29 @@ public class GridController : MonoBehaviour
                 grid[x, y] = newNode;
             }
         }
+    }
+
+    List<Vector2Int> FindPath()
+    {
+        List<Vector2Int> path = new List<Vector2Int>();
+        int currentX = 0;
+        int currentY = 0;
+
+        while (currentX < gridSizeX - 1 || currentY < gridSizeY - 1)
+        {
+            path.Add(new Vector2Int(currentX, currentY));
+
+            if (Random.value > 0.5f && currentX < gridSizeX - 1)
+            {
+                currentX++;
+            }
+            else if (currentY < gridSizeY - 1)
+            {
+                currentY++;
+            }
+        }
+
+        path.Add(new Vector2Int(gridSizeX - 1, gridSizeY - 1));
+        return path;
     }
 }
