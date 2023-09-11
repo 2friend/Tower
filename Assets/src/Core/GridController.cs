28f25144c114817ae529f13616ce46c279348667
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-09-11 19:18:07 +0300

    TOW-47 generation of the path on the map through the tile constructor

--- Assets/src/Core/GridController.cs
+++ Assets/src/Core/GridController.cs
@@ -45,6 +45,15 @@ void GenerateGrid()
                     break;
                 }
             }
+            bool leftHasPath = (x > 0 && pathList.Exists(p => p.x == x - 1 && p.y == y));
+            bool rightHasPath = (x < gridSizeX - 1 && pathList.Exists(p => p.x == x + 1 && p.y == y));
+            bool topHasPath = (y < gridSizeY - 1 && pathList.Exists(p => p.x == x && p.y == y + 1));
+            bool downHasPath = (y > 0 && pathList.Exists(p => p.x == x && p.y == y - 1));
+
+            bool topLeftCorner = (x > 0 && y < gridSizeY - 1 && pathList.Exists(p => p.x == x - 1 && p.y == y + 1));
+            bool topRightCorner = (x < gridSizeX - 1 && y < gridSizeY - 1 && pathList.Exists(p => p.x == x + 1 && p.y == y + 1));
+            bool bottomLeftCorner = (x > 0 && y > 0 && pathList.Exists(p => p.x == x - 1 && p.y == y - 1));
+            bool bottomRightCorner = (x < gridSizeX - 1 && y > 0 && pathList.Exists(p => p.x == x + 1 && p.y == y - 1));
 
             if (isPath)
             {
@@ -52,9 +61,45 @@ void GenerateGrid()
                 waypoints.Add(newNode.transform);
                 nodeComponent.haveSomething = true;
             }
+            else if (leftHasPath && topHasPath || leftHasPath && downHasPath || downHasPath && rightHasPath || topHasPath && rightHasPath) 
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[0];
+            }
+            else if (leftHasPath && !topHasPath && !rightHasPath && !downHasPath)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[2]; 
+            }
+             else if (rightHasPath && !topHasPath && !leftHasPath && !downHasPath)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[3]; 
+            }
+              else if (topHasPath && !rightHasPath && !leftHasPath && !downHasPath)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[4]; 
+            }
+              else if (downHasPath && !rightHasPath && !leftHasPath && !topHasPath)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[5]; 
+            }
+              else if (topLeftCorner)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[6]; 
+            }
+             else if (topRightCorner)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[7]; 
+            }
+             else if (bottomLeftCorner)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[8]; 
+            }
+             else if (bottomRightCorner)
+            {
+                nodeComponent.spriteRenderer.sprite = sprites[9]; 
+            }
             else
             {
-                nodeComponent.spriteRenderer.sprite = sprites[1]; 
+                nodeComponent.spriteRenderer.sprite = sprites[1];
             }
 
             newNode.gameObject.name = "X: " + x + " Y: " + y;
